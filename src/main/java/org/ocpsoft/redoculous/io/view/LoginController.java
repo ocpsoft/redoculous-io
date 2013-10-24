c5a7d5e143d0539286a565ed3b42533fc533427f
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-10-23 23:29:07 -0400

    Fixed login redirect encoding. Added first repository view page

--- src/main/java/org/ocpsoft/redoculous/io/view/LoginController.java
+++ src/main/java/org/ocpsoft/redoculous/io/view/LoginController.java
@@ -1,6 +1,8 @@
 package org.ocpsoft.redoculous.io.view;
 
 import java.io.Serializable;
+import java.util.List;
+import java.util.Map.Entry;
 
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
@@ -9,10 +11,14 @@ import javax.inject.Named;
 import org.ocpsoft.redoculous.io.util.Threads;
 import org.ocpsoft.redoculous.io.util.jsf.FacesMessages;
 import org.ocpsoft.rewrite.faces.navigate.Navigate;
+import org.ocpsoft.rewrite.servlet.util.QueryStringBuilder;
+import org.ocpsoft.urlbuilder.Address;
+import org.ocpsoft.urlbuilder.AddressBuilder;
 import org.picketlink.Identity;
 import org.picketlink.Identity.AuthenticationResult;
 
 @Named
+@SuppressWarnings("deprecation")
 public class LoginController implements Serializable
 {
    private static final long serialVersionUID = 4164173751827686256L;
@@ -41,8 +47,20 @@ public class LoginController implements Serializable
       if (target == null || target.isEmpty())
       {
          target = "/account/dashboard";
+         return Navigate.to(target);
       }
-      return Navigate.to(target);
+      else
+      {
+         Address address = AddressBuilder.create(target);
+         Navigate navigate = Navigate.to(address.getPath());
+         for (Entry<String, List<String>> param : QueryStringBuilder.createNew().addParameters(address.getQuery())
+                  .getParameterMap().entrySet()) {
+            for (String value : param.getValue()) {
+               navigate.with(param.getKey(), value);
+            }
+         }
+         return navigate;
+      }
 
    }
 
---
+++ src/main/java/org/ocpsoft/redoculous/io/view/account/repositories/RepositoryController.java
@@ -0,0 +1,139 @@
+package org.ocpsoft.redoculous.io.view.account.repositories;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+import javax.ejb.Stateful;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.ValidatorException;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.persistence.EntityManager;
+
+import org.ocpsoft.redoculous.io.model.account.UserProfile;
+import org.ocpsoft.redoculous.io.model.repositories.SourceRepository;
+import org.ocpsoft.redoculous.io.util.jsf.FacesMessages;
+import org.ocpsoft.rewrite.faces.navigate.Navigate;
+
+@Named
+@Stateful
+@RequestScoped
+public class RepositoryController implements Serializable
+{
+   private static final long serialVersionUID = -2583174292087675121L;
+
+   private String repoUrl;
+
+   private SourceRepository repository = new SourceRepository();
+
+   @Inject
+   private EntityManager em;
+
+   @Inject
+   private UserProfile profile;
+
+   public Object add()
+   {
+      repository.setApiKey(UUID.randomUUID().toString());
+      repository.setOwner(null);
+      profile.getRepositories().add(repository);
+      repository.setOwner(profile);
+
+      em.persist(repository);
+      em.merge(profile);
+      return Navigate.to("/account/repositories");
+   }
+
+   public Object save()
+   {
+      em.merge(repository);
+      return Navigate.to("/account/repositories/view").with("repo", repoUrl);
+   }
+
+   public void loadRepo()
+   {
+      for (SourceRepository repo : profile.getRepositories()) {
+         if (!repo.isDeleted() && repo.getUrl().equals(repoUrl))
+         {
+            repository = repo;
+            break;
+         }
+      }
+
+      if (!repository.isPersistent())
+         FacesContext.getCurrentInstance().getApplication().getNavigationHandler()
+                  .handleNavigation(FacesContext.getCurrentInstance(), "", "/404");
+   }
+
+   public void validateNewName(FacesContext context, UIComponent component, Object value)
+   {
+      if (value instanceof String)
+      {
+         String repoUrl = (String) value;
+         if (repoUrl.length() > 32)
+         {
+            throw new ValidatorException(FacesMessages.error("Name must be shorter than 32 characters."));
+         }
+
+         for (SourceRepository repo : profile.getRepositories()) {
+            if (repo.getUrl().equals(repoUrl))
+            {
+               throw new ValidatorException(
+                        FacesMessages.error("A repository with the given URL is already registered."));
+            }
+         }
+      }
+   }
+
+   public void validateNewUrl(FacesContext context, UIComponent component, Object value)
+   {
+      if (value instanceof String)
+      {
+         String repoUrl = (String) value;
+
+         for (SourceRepository repo : profile.getRepositories()) {
+            if (repo.getUrl().equals(repoUrl))
+            {
+               throw new ValidatorException(
+                        FacesMessages.error("A repository with the given URL is already registered."));
+            }
+         }
+      }
+   }
+
+   public void validateName(FacesContext context, UIComponent component, Object value)
+   {
+      if (value instanceof String)
+      {
+         if (((String) value).length() > 32)
+         {
+            throw new ValidatorException(FacesMessages.error("Name must be shorter than 32 characters."));
+         }
+      }
+   }
+
+   public void validateUrl(FacesContext context, UIComponent component, Object value)
+   {}
+
+   public SourceRepository getRepository()
+   {
+      return repository;
+   }
+
+   public void setRepository(SourceRepository repository)
+   {
+      this.repository = repository;
+   }
+
+   public String getRepoUrl()
+   {
+      return repoUrl;
+   }
+
+   public void setRepoUrl(String repoUrl)
+   {
+      this.repoUrl = repoUrl;
+   }
+}
