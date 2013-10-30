0904549bfc45fb32b24c7244ec5ad2c52619181b
Lincoln Baxter, III <lincolnbaxter@gmail.com>
2013-10-30 18:17:42 -0400

    Implemented poll cancellation

--- src/main/webapp/resources/tags/poll.js
+++ src/main/webapp/resources/tags/poll.js
@@ -1,50 +1,71 @@
-if (!window.redoculoustags) {
-	var redoculoustags = {};
+if (!window.redoculoustags)
+{
+   var redoculoustags = {};
 }
 
-if (!redoculoustags.poll) {
-	redoculoustags.poll = {};
+if (!redoculoustags.poll)
+{
+   redoculoustags.poll = {};
 }
 
-if (!redoculoustags.poll.init) {
-	redoculoustags.poll.init = function init(id, inc, to, rend) {
-		var componentID = id;
-		var increment = inc;
-		var timeout = to;
-		var elapsed = 0;
-		var token = null;
-		var render = rend;
+if (!redoculoustags.poll.init)
+{
+   redoculoustags.poll.init = function init(id, inc, to, rend, canc)
+   {
+      var componentID = id;
+      var increment = inc;
+      var timeout = to;
+      var elapsed = 0;
+      var token = null;
+      var render = rend;
+      var cancel = canc;
 
-		// If increment isn't set, or it's less than some reasonable value (50)
-		// default to 200ms
-		if (isNaN(increment) || increment <= 50) {
-			increment = 200;
-		}
+      if (typeof cancel == 'undefined' && cancel != "")
+      {
+         cancel = false;
+      }
 
-		// If timeout isn't set, default to no timeout
-		if (isNaN(timeout) || timeout == 0) {
-			timeout = -1;
-		}
+      // If increment isn't set, or it's less than some reasonable value (50)
+      // default to 200ms
+      if (isNaN(increment) || increment <= 50)
+      {
+         increment = 200;
+      }
 
-		var poll = function poll() {
-			jsf.ajax.request(componentID, null, {
-				render : render
-			});
-			if (timeout != -1) {
-				// Not an accurate timeout - but simple to compute
-				elapsed += increment;
-				if (elapsed > timeout) {
-					window.clearInterval(token);
-				}
-			}
-		};
+      // If timeout isn't set, default to no timeout
+      if (isNaN(timeout) || timeout == 0)
+      {
+         timeout = -1;
+      }
 
-		token = window.setInterval(poll, increment);
+      var poll = function poll()
+      {
+         // Not an accurate timeout - but simple to compute
+         elapsed += Number(increment);
+         if (eval(cancel))
+         {
+            window.clearInterval(token);
+         }
+         else if (timeout != -1 && elapsed > timeout)
+         {
+            window.clearInterval(token);
+         }
+         else
+         {
+            jsf.ajax.request(componentID, null, {
+               render : render
+            });
+         }
+      };
 
-		return function cancelPoll(data) {
-			if (data.source.id == componentID) {
-				window.clearInterval(token);
-			}
-		};
-	}
+      token = window.setInterval(poll, increment);
+
+      return function cancelPoll(data)
+      {
+         if (data.source.id == componentID)
+         {
+            window.clearInterval(token);
+         }
+      };
+   };
 }
